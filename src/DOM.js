75c06d680fe4dd28ebb59ef02f16a110e55e6620
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-04-06 03:31:58 +0000

    updated playRound function

--- src/DOM.js
+++ src/DOM.js
@@ -1,27 +1,39 @@
 import { Player } from "./Player.js";
 function DOM() {
- 
-  const updateBoardDisplay = (gameboard, playerNumber) => {
+  const updateBoardDisplay = (gameboard, playerNumber, isGameStarted) => {
     const board = gameboard.getBoard();
     const boardElement =
-      playerNumber == 1
+      playerNumber == 0
         ? document.querySelector(".humanBoard")
         : document.querySelector(".computerBoard");
     const rows = 10;
     const columns = 10;
-    for (let i = 0; i < rows; ++i) {
-      const rowdiv = document.createElement("div");
-      rowdiv.classList.add("row");
-      boardElement.appendChild(rowdiv);
+    if (!isGameStarted) {
 
-      for (let j = 0; j < columns; ++j) {
-        const valOnCell = board[i][j].getValue();
-        const celldiv = document.createElement("div");
-        celldiv.classList.add("cell");
-        celldiv.dataset.row = i;
-        celldiv.dataset.column = j;
-        celldiv.textContent = valOnCell;
-        rowdiv.appendChild(celldiv);
+      for (let i = 0; i < rows; ++i) {
+        const rowdiv = document.createElement("div");
+        rowdiv.classList.add("row");
+        boardElement.appendChild(rowdiv);
+
+        for (let j = 0; j < columns; ++j) {
+          const valOnCell = board[i][j].getValue();
+          const celldiv = document.createElement("div");
+          celldiv.classList.add("cell");
+          celldiv.dataset.row = i;
+          celldiv.dataset.column = j;
+          celldiv.textContent = valOnCell;
+          rowdiv.appendChild(celldiv);
+        }
+      }
+       
+    } else {
+      for (let i = 0; i < rows; ++i) {
+        for (let j = 0; j < column; ++j) {
+          // select the cell with dataset values i, j and some class name and that it should be descendent of computerBoard div. How ? 
+          const cell = document.querySelector(
+            `.computerBoard [data-row="${i}"]`
+          );
+        }
       }
     }
   };
@@ -40,42 +52,102 @@ function DOM() {
     gameboard2.placeShip(3, 2, 3);
     gameboard2.placeShip(3, 4, 2);
 
-    updateBoardDisplay(gameboard1, 1);
-    updateBoardDisplay(gameboard2, 2);
+    updateBoardDisplay(gameboard1, 0);
+    updateBoardDisplay(gameboard2, 1);
+
     return { player1, player2, gameboard1, gameboard2 };
   };
-  setupNewGame();
+  // setupNewGame();
 
   const gameController = () => {
     let { player1, player2, gameboard1, gameboard2 } = setupNewGame();
+
     const players = [
       {
-        name: player1
+        name: player1,
+        gameboard: gameboard1,
       },
       {
-        name: player2
-      }
+        name: player2,
+        gameboard: gameboard2,
+      },
     ];
-    let activePlayer = players[0];
+
+    let idx = 0;
+    let activePlayer = players[idx];
+
+    const turnDiv = document.querySelector(".turn");
+    turnDiv.textContent = `It's your turn now, play your move`;
+
     const switchPlayerTurn = () => {
-      activePlayer = (activePlayer == players[0] ? players[1] : players[0]);
+      activePlayer = activePlayer == players[0] ? players[1] : players[0];
     };
+
     const getActivePlayer = () => {
       return activePlayer;
     };
-    const board = document.querySelector("#board");
-    board.addEventListener('click', (event) => {
-      const cellDiv = event.target;
-      const row = cellDiv.row;
-      const column = cellDiv.column;
-      playRound(row, column);
-    });
-    const playRound = (row, column) => {
-      
+
+    const checkWin = () => {
+      if (players[idx].gameboard.areAllShipsSunk()) {
+        return true;
+      } else {
+        return false;
+      }
     };
-    return {players, switchPlayerTurn, getActivePlayer, playRound};
+
+    const computerboard = document.querySelector(".computerBoard");
+
+    computerboard.addEventListener("click", (event) => {
+      const cellDiv = event.target;
+      const row = cellDiv.dataset.row;
+      const column = cellDiv.dataset.column;
+      playRound(row, column, cellDiv);
+    });
+
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+    const getRandomInt = (min, max) => {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    const playRound = (row, column, cellDiv) => {
+
+      idx = 0;
+      players[1].gameboard.receiveAttack(row, column);
+      updateBoardDisplay(players[1].gameboard, idx);
+      if (checkWin()) {
+        prompt(`Congrats, You won the game`);
+      } else {
+
+        idx = 1;
+        turnDiv.textContent = `It's computer's move now`;
+
+        const computerMove = async () => {
+
+          await delay(5000);
+          let computerChoice = [0, 0];
+          computerChoice[0] = getRandomInt(0,9);
+          computerChoice[1] = getRandomInt(0,9);
+
+          players[0].gameboard.receiveAttack(computerChoice[0], computerChoice[1]);
+          updateBoardDisplay(players[0].gameboard, idx);
+
+          if (checkWin()) {
+            prompt(`You lose, computer won the game.`);
+          }
+
+        };
+
+        computerMove();
+      }
+    };
+    return { players, switchPlayerTurn, getActivePlayer, checkWin, playRound };
+
   };
+  gameController();
+
   return { setupNewGame, updateBoardDisplay };
 }
 export { DOM };
- 
